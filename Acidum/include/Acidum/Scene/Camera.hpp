050c5458bb044ad356b233418c913007045ba915
barlin41k <barlin41k.dev@gmail.com>
2026-06-12 15:23:45 +0300

    fix: fixed multiple view matrix calculation

--- Acidum/include/Acidum/Scene/Camera.hpp
+++ Acidum/include/Acidum/Scene/Camera.hpp
@@ -9,17 +9,19 @@ class Camera {
 public:
     Camera(float fov, float aspectRatio, float nearClip, float farClip);
 
-    void setPosition(const glm::vec3& position) { m_position = position; recalculateViewMatrix(); }
-    void setRotation(float pitch, float yaw) { m_pitch = pitch; m_yaw = yaw; recalculateViewMatrix(); }
+    void setPosition(const glm::vec3& position);
+    void setRotation(float pitch, float yaw);
     void setAspectRatio(float aspectRatio);
 
     const glm::vec3& getFront() const { return m_front; }
     const glm::vec3& getRight() const { return m_right; }
     const glm::vec3& getUp() const { return m_up; }
 
-    const glm::mat4& getViewMatrix() const { return m_viewMatrix; }
+    const glm::mat4& getViewMatrix();
     const glm::mat4& getProjectionMatrix() const { return m_projectionMatrix; }
 private:
+    bool m_isViewMatrixDirty = true;
+
     glm::vec3 m_position{0.0f, 0.0f, 3.0f};
     
     float m_pitch = 0.0f;
@@ -36,6 +38,8 @@ private:
 
     glm::mat4 m_viewMatrix {1.0f};
     glm::mat4 m_projectionMatrix {1.0f};
+
+    void updateVectors();
 
     void recalculateViewMatrix();
     void recalculateProjectionMatrix();
--- Acidum/src/Scene/Camera.cpp
+++ Acidum/src/Scene/Camera.cpp
@@ -5,11 +5,48 @@ namespace Acidum {
 Camera::Camera(float fov, float aspectRatio, float nearClip, float farClip)
     : m_fov(fov), m_aspectRatio(aspectRatio), m_nearClip(nearClip), m_farClip(farClip)
 {
+    updateVectors();
     recalculateViewMatrix();
     recalculateProjectionMatrix();
 }
 
+const glm::mat4& Camera::getViewMatrix() {
+    if (m_isViewMatrixDirty) {
+        recalculateViewMatrix();
+        m_isViewMatrixDirty = false;
+    }
+
+    return m_viewMatrix;
+}
+
 void Camera::recalculateViewMatrix() {
+    m_viewMatrix = glm::lookAt(m_position, m_position + m_front, m_up);
+}
+
+void Camera::recalculateProjectionMatrix() {
+    m_projectionMatrix = glm::perspective(glm::radians(m_fov), m_aspectRatio, m_nearClip, m_farClip);
+}
+
+void Camera::setPosition(const glm::vec3& position) {
+    m_position = position;
+    m_isViewMatrixDirty = true;
+}
+
+void Camera::setRotation(float pitch, float yaw) {
+    m_pitch = pitch;
+    m_yaw = yaw;
+    updateVectors();
+    m_isViewMatrixDirty = true;
+}
+
+void Camera::setAspectRatio(float aspectRatio) {
+    if (m_aspectRatio != aspectRatio) {
+        m_aspectRatio = aspectRatio; 
+        recalculateProjectionMatrix();
+    }
+}
+
+void Camera::updateVectors() {
     glm::vec3 front;
 
     double yawRad = glm::radians(m_yaw);
@@ -28,19 +65,6 @@ void Camera::recalculateViewMatrix() {
     m_front = glm::normalize(front);
     m_right = glm::normalize(glm::cross(m_front, m_worldUp));
     m_up = glm::normalize(glm::cross(m_right, m_front));
-
-    m_viewMatrix = glm::lookAt(m_position, m_position + m_front, m_up);
-}
-
-void Camera::recalculateProjectionMatrix() {
-    m_projectionMatrix = glm::perspective(glm::radians(m_fov), m_aspectRatio, m_nearClip, m_farClip);
-}
-
-void Camera::setAspectRatio(float aspectRatio) {
-    if (m_aspectRatio != aspectRatio) {
-        m_aspectRatio = aspectRatio; 
-        recalculateProjectionMatrix();
-    }
 }
 
 } // namespace Acidum
