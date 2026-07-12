2b3cebe44631dcca7643ac80348a60aecb049aa7
barlin41k <barlin41k.dev@gmail.com>
2026-07-12 13:15:40 +0300

    style: minor style change

--- Acidum/src/Core/Platform/Linux/PlatformUtils.cpp
+++ Acidum/src/Core/Platform/Linux/PlatformUtils.cpp
@@ -8,8 +8,7 @@ namespace Acidum::Platform {
 std::filesystem::path GetExecutableDir() {
     char result[PATH_MAX];
     ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
-    if (count != -1)
-        return std::filesystem::path(std::string_view(result, static_cast<size_t>(count))).parent_path();
+    if (count != -1) return std::filesystem::path(std::string_view(result, static_cast<size_t>(count))).parent_path();
     return {};
 }
 
