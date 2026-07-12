3814eaef20fb119d6fb1361ffb7cb9e347b1ec7c
barlin41k <barlin41k.dev@gmail.com>
2026-07-12 13:15:26 +0300

    refactor(camera): remove magic numbers

--- Acidum/include/Acidum/Scene/Camera.hpp
+++ Acidum/include/Acidum/Scene/Camera.hpp
@@ -25,12 +25,9 @@ private:
 
     glm::vec3 m_position{ 0.0f, 0.0f, 3.0f };
     
+    float m_fov, m_aspectRatio, m_nearClip, m_farClip;
     float m_pitch = 0.0f;
     float m_yaw = -90.0f;
-    float m_fov = 45.0f;
-    float m_aspectRatio = 1.0f;
-    float m_nearClip = 0.1f;
-    float m_farClip = 100.0f;
 
     glm::vec3 m_front { 0.0f, 0.0f, -1.0f };
     glm::vec3 m_right { 1.0f, 0.0f, 0.0f };
--- Acidum/src/Scene/Camera.cpp
+++ Acidum/src/Scene/Camera.cpp
@@ -3,7 +3,10 @@
 namespace Acidum {
 
 Camera::Camera(float fov, float aspectRatio, float nearClip, float farClip)
-    : m_fov(fov), m_aspectRatio(aspectRatio), m_nearClip(nearClip), m_farClip(farClip)
+    : m_fov(fov),
+      m_aspectRatio(aspectRatio),
+      m_nearClip(nearClip),
+      m_farClip(farClip)
 {
     updateVectors();
     recalculateViewMatrix();
